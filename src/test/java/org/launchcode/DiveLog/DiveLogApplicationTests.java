08aeef12e09e3048a6d2d9815917cc58c4e8c413
Catherine <netsurfer1193@gmail.com>
2019-11-25 12:07:01 -0500

    initial commit

---
+++ src/test/java/org/launchcode/DiveLog/DiveLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.DiveLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
