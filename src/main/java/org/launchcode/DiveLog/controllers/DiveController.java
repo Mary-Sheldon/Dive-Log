dd691655d941d9da74161fc9defbe7a33af2cc01
Catherine <netsurfer1193@gmail.com>
2019-11-25 16:47:35 -0500

    This is a working set up connected to database

--- src/main/java/org/launchcode/DiveLog/controllers/DiveController.java
+++ src/main/java/org/launchcode/DiveLog/controllers/DiveController.java
@@ -4,6 +4,9 @@ import org.launchcode.DiveLog.models.data.DiveDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("")
@@ -11,4 +14,16 @@ public class DiveController {
 
     @Autowired
     private DiveDao diveDao;
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index(HttpServletRequest request) {
+        /* I need to put "name" in the parenthesis below */
+        String name = request.getParameter("name");
+
+        if (name == null) {
+            name = "World!";
+        }
+        return "Hello " + name;
+    }
 }
--- src/main/java/org/launchcode/DiveLog/models/Dive.java
+++ src/main/java/org/launchcode/DiveLog/models/Dive.java
@@ -1,7 +1,13 @@
 package org.launchcode.DiveLog.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Dive {
+
+    @Id
+    @GeneratedValue
+    private int id;
 }
